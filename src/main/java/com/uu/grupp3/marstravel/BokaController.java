165c6c1dd4ef5008c7a7cdb9cd365b08ecff2c6f
BUNBUN <natalie.franzenon01@student.uu.se>
2024-02-21 11:59:50 +0100

    Skapade boka sida

---
+++ src/main/java/com/uu/grupp3/marstravel/BokaController.java
@@ -0,0 +1,14 @@
+package com.uu.grupp3.marstravel;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class BokaController {
+
+        @FXML
+        private Label HeaderMT;
+
+        @FXML
+        private Button btnBoka;
+
+}
