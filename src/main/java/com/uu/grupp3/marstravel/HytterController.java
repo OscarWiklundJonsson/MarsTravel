09c8b0fc1faee8869aa6d0a4fcee406c26d03f6a
BUNBUN <natalie.franzenon01@student.uu.se>
2024-02-21 22:02:53 +0100

    Skapade en vy för hytter

---
+++ src/main/java/com/uu/grupp3/marstravel/HytterController.java
@@ -0,0 +1,69 @@
+package com.uu.grupp3.marstravel;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.shape.Circle;
+
+public class HytterController {
+
+    @FXML
+    private Button btnEcoInfo;
+
+    @FXML
+    private Button btnInsideInfo;
+
+    @FXML
+    private Button btnNÄSTA;
+
+    @FXML
+    private Button btnSleepInfo;
+
+    @FXML
+    private Button btnSpacesideInfo;
+
+    @FXML
+    private Button btnSvitInfo;
+
+    @FXML
+    private Circle cVarukorgen;
+
+    @FXML
+    private Label lblEcoPris;
+
+    @FXML
+    private Label lblInsidePris;
+
+    @FXML
+    private Label lblMTheader;
+
+    @FXML
+    private Label lblPris;
+
+    @FXML
+    private Label lblSleepPris;
+
+    @FXML
+    private Label lblSpacesidePris;
+
+    @FXML
+    private Label lblSvitPris;
+
+    @FXML
+    private Label lblhyttalternativ;
+
+    @FXML
+    private RadioButton rbtnEco;
+
+    @FXML
+    private RadioButton rbtnInside;
+
+    @FXML
+    private RadioButton rbtnSleep;
+
+    @FXML
+    private RadioButton rbtnSpaceside;
+
+    @FXML
+    private RadioButton rbtnSvit;
+
+}
