b7f9813e3b266b1d8e1e1d8fa1733cf04ccf15f7
BUNBUN <natalie.franzenon01@student.uu.se>
2024-02-23 13:01:55 +0100

    Update MatPaketController.java

--- src/main/java/com/uu/grupp3/marstravel/controllers/MatPaketController.java
+++ src/main/java/com/uu/grupp3/marstravel/controllers/MatPaketController.java
@@ -1,5 +1,137 @@
 package com.uu.grupp3.marstravel.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.shape.Circle;
+
 public class MatPaketController {
+
+    @FXML
+    private Button btnEcoInfo;
+
+    @FXML
+    private Button btnEcoInfo1;
+
+    @FXML
+    private Button btnEcoInfo11;
+
+    @FXML
+    private Button btnEcoInfo111;
+
+    @FXML
+    private Button btnEcoInfo1111;
+
+    @FXML
+    private Button btnInsideInfo;
+
+    @FXML
+    private Button btnNÄSTA;
+
+    @FXML
+    private Button btnSleepInfo;
+
+    @FXML
+    private Button btnSpacesideInfo;
+
+    @FXML
+    private Button btnSvitInfo;
+
+    @FXML
+    private Button btnVALJAbetalkort;
+
+    @FXML
+    private Button btnVALJAevenemangdit;
+
+    @FXML
+    private Button btnVALJAevenemanghem;
+
+    @FXML
+    private Button btnVALJAhalsoforsakring;
+
+    @FXML
+    private Button btnVALJAhotellmars;
+
+    @FXML
+    private Button btnVALJAhyttdit;
+
+    @FXML
+    private Button btnVALJAhytthem;
+
+    @FXML
+    private Button btnVALJAkundinfo;
+
+    @FXML
+    private Button btnVALJAmatpaketdit;
+
+    @FXML
+    private Button btnVALJAmatpakethem;
+
+    @FXML
+    private Circle cVarukorgen;
+
+    @FXML
+    private Label lblEcoPris;
+
+    @FXML
+    private Label lblEcoPris1;
+
+    @FXML
+    private Label lblEcoPris2;
+
+    @FXML
+    private Label lblEcoPris21;
+
+    @FXML
+    private Label lblInsidePris;
+
+    @FXML
+    private Label lblInsidePris1;
+
+    @FXML
+    private Label lblMTheader;
+
+    @FXML
+    private Label lblPris;
+
+    @FXML
+    private Label lblSleepPris;
+
+    @FXML
+    private Label lblSpacesidePris;
+
+    @FXML
+    private Label lblSvitPris;
+
+    @FXML
+    private Label lblhyttalternativ;
+
+    @FXML
+    private RadioButton rbtnEco;
+
+    @FXML
+    private RadioButton rbtnEco1;
+
+    @FXML
+    private RadioButton rbtnEco11;
+
+    @FXML
+    private RadioButton rbtnEco2;
+
+    @FXML
+    private RadioButton rbtnInside;
+
+    @FXML
+    private RadioButton rbtnInside1;
+
+    @FXML
+    private RadioButton rbtnSleep;
+
+    @FXML
+    private RadioButton rbtnSpaceside;
+
+    @FXML
+    private RadioButton rbtnSvit;
 
 }
