e2ca882a1c31c7cca14b773efea32bade0984fe0
hugo <160583187+mrmanhugo@users.noreply.github.com>
2024-02-27 13:34:00 +0100

    sidoknapp på alla sidorna

--- src/main/java/com/uu/grupp3/marstravel/controllers/HytterHemController.java
+++ src/main/java/com/uu/grupp3/marstravel/controllers/HytterHemController.java
@@ -1,9 +1,12 @@
 package com.uu.grupp3.marstravel.controllers;
 
 import com.uu.grupp3.marstravel.database.DatabaseReciveInformation;
+import com.uu.grupp3.marstravel.services.SideBarButtons;
 import com.uu.grupp3.marstravel.services.StoreTravelChoices;
 import com.uu.grupp3.marstravel.services.NextButton;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
@@ -140,5 +143,51 @@ public class HytterHemController {
             Stage stage = (Stage) btnNASTA.getScene().getWindow();
             nextButton.nextButton("com/uu/grupp3/marstravel/matpaketHem.fxml", stage);
         });
+    }
+    private SideBarButtons sideBarButtons = new SideBarButtons();
+
+    @FXML
+    private void SideBarButtons(ActionEvent event) {
+        String fxmlPath = null;
+
+        if (event.getSource() == btnVALJAevenemangdit) {
+            fxmlPath = "/com/uu/grupp3/marstravel/evenemang.fxml";
+            System.out.println("Evenemang");
+        } else if (event.getSource() == btnVALJAmatpaketdit) {
+            fxmlPath = "/com/uu/grupp3/marstravel/matpaket.fxml";
+            System.out.println("Matpaket");
+        } else if (event.getSource() == btnVALJAhyttdit) {
+            fxmlPath = "/com/uu/grupp3/marstravel/hytter.fxml";
+            System.out.println("Hytter");
+        } else if (event.getSource() == btnVALJAresedatum) {
+            fxmlPath = "/com/uu/grupp3/marstravel/resedatum.fxml";
+            System.out.println("Resedatum");
+        } else if (event.getSource() == btnVALJAkundinfo) {
+            fxmlPath = "/com/uu/grupp3/marstravel/kundinformation.fxml";
+            System.out.println("Kundinformation");
+        } else if (event.getSource() == btnVALJAbetalkort) {
+            fxmlPath = "/com/uu/grupp3/marstravel/betalkort.fxml";
+            System.out.println("Betalkort");
+        } else if (event.getSource() == btnVALJAhalsoforsakring) {
+            fxmlPath = "/com/uu/grupp3/marstravel/halsoforsakring.fxml";
+            System.out.println("Hälsförsäkring");
+        } else if (event.getSource() == btnVALJAevenemanghem) {
+            fxmlPath = "/com/uu/grupp3/marstravel/evenemanghem.fxml";
+            System.out.println("Evenemang hem");
+        } else if (event.getSource() == btnVALJAmatpakethem) {
+            fxmlPath = "/com/uu/grupp3/marstravel/matpakethem.fxml";
+            System.out.println("Matpaket hem");
+        } else if (event.getSource() == btnVALJAhytthem) {
+            fxmlPath = "/com/uu/grupp3/marstravel/hytterhem.fxml";
+            System.out.println("Hytter hem");
+        } else if (event.getSource() == btnVALJAhotellmars) {
+            fxmlPath = "/com/uu/grupp3/marstravel/hotellmars.fxml";
+            System.out.println("Hotell Mars");
+        }
+
+        if (fxmlPath != null) {
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            sideBarButtons.sideBarButton(fxmlPath, stage);
+        }
     }
 }
