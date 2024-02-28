016dcdf93552cee8baae2926d027b0c703b15b83
Oscar Wiklund Jonsson <oscar.wiklund-jonsson.2717@student.uu.se>
2024-02-28 17:17:05 +0100

    stuff

--- src/main/java/com/uu/grupp3/marstravel/services/StoreTravelChoices.java
+++ src/main/java/com/uu/grupp3/marstravel/services/StoreTravelChoices.java
@@ -12,308 +12,193 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 public class StoreTravelChoices {
+    private static final String FILE_NAME = "travelChoices.txt"; // Filnamnet ;)
     private static boolean isFirstRun = true;
-    String fileName = "travelChoices.txt";
 
     /**
      * Stores the selected radio button in a file
-     * @param group (ToggleGroup) the group of radio buttons
+     * @param group the toggle group
+     * @param prefix the prefix to be written to the file ( e.g. "Matpaket: " )
      */
     public void storeSelectedRadioButton(ToggleGroup group, String prefix) {
         RadioButton selectedRadioButton = (RadioButton) group.getSelectedToggle();
         String selectedRadioButtonValue = selectedRadioButton == null ? "---" : selectedRadioButton.getText();
         writeToFile(prefix + selectedRadioButtonValue);
     }
+
+    /**
+     * Stores the selected date in a file
+     * @param date the selected date
+     */
     public void storeDate(String date) {
         writeToFile(date);
     }
 
+    /**
+     * Stores the selected choice box in a file
+     * @param prefix the prefix to be written to the file ( e.g. "Hytt: " )
+     * @param choiceBox the choice box
+     */
     public void storeChoiceBox(String prefix, ChoiceBox<String> choiceBox) {
         String selectedValue = choiceBox.getValue();
         writeToFile(prefix + selectedValue);
     }
 
     /**
-     * Writes the content to a file
-     * @param content (String) the content to write to the file (HyttAlternativ, MatPaket osv)
+     * Stores the selected check box in a file
+     * @param content the content to be written to the file ( e.g. "Hälsoförsäkring: Hälsoförsäkring" )
      */
     public void writeToFile(String content) {
-        String fileName = "travelChoices.txt";
-        Path path = Paths.get(fileName);
-        int kundNr = 1; // TODO: anpassa till något lämpligt
+        Path path = Paths.get(FILE_NAME);
+        int kundNr = 1; // TODO: anpassa till något lämpligt sätt att hämta kundnummer
 
         try (BufferedWriter writer = Files.newBufferedWriter(path, StandardOpenOption.APPEND)) {
             if (isFirstRun) {
-                writer.write("---------Kund-" + kundNr + "----------\n");
+                writer.write(STR."---------Kund-\{kundNr}----------\n");
                 isFirstRun = false;
             }
-            writer.write(content + "\n");
+            writer.write(STR."\{content}\n");
+        } catch (IOException e) {
+            e.printStackTrace(); // Robust logging? nej tack
+        }
+    }
+
+    /**
+     * Reads the file and returns the first line containing the search string
+     * @param searchString
+     * @return the first line containing the search string
+     */
+    public String getLineContaining(String searchString) {
+        Path path = Paths.get(FILE_NAME);
+
+        try (BufferedReader reader = Files.newBufferedReader(path)) {
+            return reader.lines()
+                    .filter(line -> line.contains(searchString))
+                    .findFirst()
+                    .orElse(null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    /**
+     * Removes the first line containing the search string
+     * @param searchString the search string
+     */
+    public void removeLineContaining(String searchString) {
+        Path path = Paths.get(FILE_NAME);
+
+        try {
+            List<String> lines = Files.readAllLines(path);
+            lines.removeIf(line -> line.contains(searchString));
+            Files.write(path, lines);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     /**
-     * Retrieves the stored date from the file
-     * @return (String) the stored date
+     * Removes all lines containing the search string
+     * @param  // får felkod om jag har en param här, så då struntar vi i det :D
      */
     public String getDate() {
-        String fileName = "travelChoices.txt";
-        Path path = Paths.get(fileName);
-
-        try (BufferedReader reader = Files.newBufferedReader(path)) {
-            List<String> lines = reader.lines().collect(Collectors.toList());
-            for (String line : lines) {
-                if (line.contains(" till ")) {
-                    return line;
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-
-    public String getMat(){
-        String fileName = "travelChoices.txt";
-        Path path = Paths.get(fileName);
-
-        try (BufferedReader reader = Files.newBufferedReader(path)) {
-            List<String> lines = reader.lines().collect(Collectors.toList());
-            for (String line : lines) {
-                if (line.contains("Matpaket: ")) {
-                    return line;
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-    public String getMatHem(){
-        String fileName = "travelChoices.txt";
-        Path path = Paths.get(fileName);
-
-        try (BufferedReader reader = Files.newBufferedReader(path)) {
-            List<String> lines = reader.lines().collect(Collectors.toList());
-            for (String line : lines) {
-                if (line.contains("MatpaketHem: ")) {
-                    return line;
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-
-    public String getHytt(){
-        String fileName = "travelChoices.txt";
-        Path path = Paths.get(fileName);
-
-        try (BufferedReader reader = Files.newBufferedReader(path)) {
-            List<String> lines = reader.lines().collect(Collectors.toList());
-            for (String line : lines) {
-                if (line.contains("Hytt: ")) {
-                    return line;
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-    public String getHyttHem(){
-        Path path = Paths.get(fileName);
-
-        try (BufferedReader reader = Files.newBufferedReader(path)) {
-            List<String> lines = reader.lines().collect(Collectors.toList());
-            for (String line : lines) {
-                if (line.contains("HyttHem: ")) {
-                    return line;
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-
-    public String getEvenemang() {
-        Path path = Paths.get(fileName);
-
-        try (BufferedReader reader = Files.newBufferedReader(path)) {
-            List<String> lines = reader.lines().collect(Collectors.toList());
-            for (String line : lines) {
-                if (line.contains("Filmpremiarer: ")) {
-                    return line;
-                }
-                if (line.contains("Teaterpremiarer: ")) {
-                    return line;
-                }
-                if (line.contains("Konserter: ")) {
-                    return line;
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-    public String getEvenemangHem() {
-        Path path = Paths.get(fileName);
-
-        try (BufferedReader reader = Files.newBufferedReader(path)) {
-            List<String> lines = reader.lines().collect(Collectors.toList());
-            for (String line : lines) {
-                if (line.contains("FilmpremiarerHem: ")) {
-                    return line;
-                }
-                if (line.contains("TeaterpremiarerHem: ")) {
-                    return line;
-                }
-                if (line.contains("KonserterHem: ")) {
-                    return line;
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-
-    public String getHealthIns() {
-        String fileName = "travelChoices.txt";
-        Path path = Paths.get(fileName);
-
-        try (BufferedReader reader = Files.newBufferedReader(path)) {
-            List<String> lines = reader.lines().collect(Collectors.toList());
-            for (String line : lines) {
-                if (line.contains("Hälsoförsäkring: ")) {
-                    return line;
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return null;
+        return getLineContaining(" till ");
     }
 
     /**
-     * Removes the stored info from the file
+     * Returns the date
+     * @return the date
      */
+    public String getMat() {
+        return getLineContaining("Matpaket: ");
+    }
+
+    /**
+     * Returns the mat hem
+     * @return the mat hem
+     */
+    public String getMatHem() {
+        return getLineContaining("MatpaketHem: ");
+    }
+
+    /**
+     * Returns the hytt
+     * @return the hytt
+     */
+    public String getHytt() {
+        return getLineContaining("Hytt: ");
+    }
+
+    /**
+     * Returns the hytt hem
+     * @return
+     */
+    public String getHyttHem() {
+        return getLineContaining("HyttHem: ");
+    }
+    /**
+     * Returns the evenemang
+     * @return the evenemang
+     */
+    public String getEvenemang() {
+        return getLineContaining("Filmpremiarer: ") != null ? getLineContaining("Filmpremiarer: ") :
+                getLineContaining("Teaterpremiarer: ") != null ? getLineContaining("Teaterpremiarer: ") :
+                        getLineContaining("Konserter: ");
+    }
+
+    /**
+     * Returns the evenemang hem
+     * @return the evenemang hem
+     */
+    public String getEvenemangHem() {
+        return getLineContaining("FilmpremiarerHem: ") != null ? getLineContaining("FilmpremiarerHem: ") :
+                getLineContaining("TeaterpremiarerHem: ") != null ? getLineContaining("TeaterpremiarerHem: ") :
+                        getLineContaining("KonserterHem: ");
+    }
+
+    // Orkar inte göra fler av dessa, koden funkar likadant för alla metoder...
+    public String getHealthIns() {
+        return getLineContaining("Hälsoförsäkring: ");
+    }
+
     public void removeDate() {
-        String fileName = "travelChoices.txt";
-        Path path = Paths.get(fileName);
-
-        try {
-            List<String> lines = Files.readAllLines(path);
-            lines.removeIf(line -> line.contains(" till "));
-            Files.write(path, lines);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        removeLineContaining(" till ");
     }
+
     public void removeHytt() {
-        String fileName = "travelChoices.txt";
-        Path path = Paths.get(fileName);
-
-        try {
-            List<String> lines = Files.readAllLines(path);
-            lines.removeIf(line -> line.contains("Hytt: "));
-            Files.write(path, lines);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        removeLineContaining("Hytt: ");
     }
+
     public void removeHyttHem() {
-        String fileName = "travelChoices.txt";
-        Path path = Paths.get(fileName);
-
-        try {
-            List<String> lines = Files.readAllLines(path);
-            lines.removeIf(line -> line.contains("HyttHem: "));
-            Files.write(path, lines);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        removeLineContaining("HyttHem: ");
     }
+
     public void removeMat() {
-        String fileName = "travelChoices.txt";
-        Path path = Paths.get(fileName);
-
-        try {
-            List<String> lines = Files.readAllLines(path);
-            lines.removeIf(line -> line.contains("Matpaket: "));
-            Files.write(path, lines);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        removeLineContaining("Matpaket: ");
     }
+
     public void removeMatHem() {
-        String fileName = "travelChoices.txt";
-        Path path = Paths.get(fileName);
-
-        try {
-            List<String> lines = Files.readAllLines(path);
-            lines.removeIf(line -> line.contains("MatpaketHem: "));
-            Files.write(path, lines);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        removeLineContaining("MatpaketHem: ");
     }
+
     public void removeEvenemang() {
-        String fileName = "travelChoices.txt";
-        Path path = Paths.get(fileName);
-
-        try {
-            List<String> lines = Files.readAllLines(path);
-            lines.removeIf(line -> line.contains("Filmpremiarer: "));
-            lines.removeIf(line -> line.contains("Teaterpremiarer: "));
-            lines.removeIf(line -> line.contains("Konserter: "));
-            Files.write(path, lines);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
+        removeLineContaining("Filmpremiarer: ");
+        removeLineContaining("Teaterpremiarer: ");
+        removeLineContaining("Konserter: ");
     }
+
     public void removeHealthIns() {
-        String fileName = "travelChoices.txt";
-        Path path = Paths.get(fileName);
-
-        try {
-            List<String> lines = Files.readAllLines(path);
-            lines.removeIf(line -> line.contains("Hälsoförsäkring: "));
-            Files.write(path, lines);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        removeLineContaining("Hälsoförsäkring: ");
     }
+
     public void removeEvenemangHem() {
-        String fileName = "travelChoices.txt";
-        Path path = Paths.get(fileName);
-
-        try {
-            List<String> lines = Files.readAllLines(path);
-            lines.removeIf(line -> line.contains("FilmpremiarerHem: "));
-            lines.removeIf(line -> line.contains("TeaterpremiarerHem: "));
-            lines.removeIf(line -> line.contains("KonserterHem: "));
-            Files.write(path, lines);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+        removeLineContaining("FilmpremiarerHem: ");
+        removeLineContaining("TeaterpremiarerHem: ");
+        removeLineContaining("KonserterHem: ");
     }
 }
