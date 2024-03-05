5b830154e65729d3e195193a4605aeec01aa4fec
felixlarssonljungsvikuu <160585011+felixlarssonljungsvikuu@users.noreply.github.com>
2024-03-05 09:45:01 +0100

    bugfix

--- src/main/java/com/uu/grupp3/marstravel/services/CharacterRestrictions.java
+++ src/main/java/com/uu/grupp3/marstravel/services/CharacterRestrictions.java
@@ -28,7 +28,7 @@ public class CharacterRestrictions {
             } else if (textField.getId().equals("tfphonenumber")) {
                 textField.setTextFormatter(createFormatter("[0-9#]*"));
             } else if (textField.getId().equals("tfemail")) {
-                textField.setTextFormatter(createFormatter("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}"));
+                textField.setTextFormatter(createFormatter("[0-9a-zA-Z\\+\\-_~]*"));
             }
         }
     }
