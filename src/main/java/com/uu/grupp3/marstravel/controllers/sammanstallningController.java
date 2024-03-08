98a5bf65a6629b42720398ece8525a0302471fa3
felixlarssonljungsvikuu <160585011+felixlarssonljungsvikuu@users.noreply.github.com>
2024-03-08 09:52:58 +0100

    Faktura omdöpning och errer korrigering

---
+++ src/main/java/com/uu/grupp3/marstravel/controllers/sammanstallningController.java
@@ -0,0 +1,11 @@
+package com.uu.grupp3.marstravel.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class sammanstallningController {
+
+}
+
+
