d151b1e74e4a2989d95d832716dd13823828fda8
BUNBUN <natalie.franzenon01@student.uu.se>
2024-03-12 13:07:40 +0100

    saves order on godkänn

--- src/main/java/com/uu/grupp3/marstravel/controllers/SparaKundController.java
+++ src/main/java/com/uu/grupp3/marstravel/controllers/SparaKundController.java
@@ -9,7 +9,6 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 import java.sql.Connection;
@@ -59,9 +58,6 @@ public class SparaKundController {
 
     @FXML
     private Button btnInfoHalsodetaljer;
-
-    @FXML
-    private Circle cVarukorgen;
 
     @FXML
     private Label lblkundinfo;
@@ -122,9 +118,7 @@ public class SparaKundController {
             storeTravelChoices.writeToFile(customerInfo);
 
             NextButton nextButton = new NextButton();
-            checkoutCartService.storeInformation();
             Stage stage = (Stage) btnKASSA.getScene().getWindow();
-            // Här skulle min metod för att spara kundinformationen till databasen finnas
             nextButton.nextButton("/com/uu/grupp3/marstravel/sammanstallning.fxml", stage); //ska skickas till sammanställningen.
         });
 
@@ -223,7 +217,7 @@ public class SparaKundController {
             alert.showAndWait();
         }
     }
-    //nästa knapp, skickar vidare till hytterHem sidan.
+    //nästa knapp.
     CheckoutCartService checkoutCartService = new CheckoutCartService();
 
     private SideBarButtons sideBarButtons = new SideBarButtons();
