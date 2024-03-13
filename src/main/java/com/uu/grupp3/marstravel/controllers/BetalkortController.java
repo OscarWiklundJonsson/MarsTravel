df25897fb42caccd66e9b790517de05f79b34bbc
BUNBUN <natalie.franzenon01@student.uu.se>
2024-03-13 10:54:21 +0100

    deleting unnecessary things

--- src/main/java/com/uu/grupp3/marstravel/controllers/BetalkortController.java
+++ src/main/java/com/uu/grupp3/marstravel/controllers/BetalkortController.java
@@ -9,7 +9,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.*;
-import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 public class BetalkortController {
